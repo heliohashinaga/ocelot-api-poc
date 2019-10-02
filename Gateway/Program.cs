d818d671eb67fdc67c7e9e5676dffb38390ceb82
Helio Higuchi Hashinaga <helio.hashinaga@clear.sale>
2019-10-02 09:35:07 -0300

    Início

---
+++ Gateway/Program.cs
@@ -0,0 +1,45 @@
+﻿using App.Metrics;
+using App.Metrics.AspNetCore;
+using App.Metrics.Formatters.InfluxDB;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using NLog.Web;
+using System;
+
+namespace OcelotTeste.Gateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(config => config.AddJsonFile("ocelot.json"))
+                .ConfigureMetricsWithDefaults((hostingContext, builder) =>
+                {
+                    builder.Report.ToInfluxDb(options =>
+                    {
+                        options.InfluxDb.BaseUri = new Uri(hostingContext.Configuration["MetricsReport:InfluxDBUrl"]);
+                        options.InfluxDb.Database = hostingContext.Configuration["MetricsReport:DataBase"];
+                        options.InfluxDb.UserName = hostingContext.Configuration["MetricsReport:InfluxDBUser"];
+                        options.InfluxDb.Password = hostingContext.Configuration["MetricsReport:InfluxDBPassword"];
+                        options.InfluxDb.CreateDataBaseIfNotExists = true;
+                        options.HttpPolicy.BackoffPeriod = TimeSpan.FromSeconds(30);
+                        options.HttpPolicy.FailuresBeforeBackoff = 5;
+                        options.HttpPolicy.Timeout = TimeSpan.FromSeconds(10);
+                        options.MetricsOutputFormatter = new MetricsInfluxDbLineProtocolOutputFormatter();
+                        options.FlushInterval = TimeSpan.FromSeconds(5);
+                    });
+                })
+                .UseMetrics()
+                .UseStartup<Startup>()
+                .UseNLog();
+        }
+            
+    }
+}
---
+++ Gateway/Startup.cs
@@ -0,0 +1,59 @@
+﻿using App.Metrics;
+using App.Metrics.Formatters.InfluxDB;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Polly;
+using System;
+
+namespace OcelotTeste.Gateway
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().AddMetrics();
+
+            services
+                .AddMetrics()
+                .AddMetricsTrackingMiddleware()
+                .AddMetricsEndpoints()
+                .AddOcelot()
+                .AddPolly();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app
+                .UseHttpsRedirection()
+                .UseMvc()
+                .UseMetricsAllMiddleware()
+                .UseMetricsAllEndpoints()
+                .UseOcelot().Wait();
+        }
+    }
+}
